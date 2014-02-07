2488bd302c250afd7631dd0b9d0cfca430912b7c
Marcel Mika <marcel.mika@rclick.cz>
2014-02-07 23:46:50 +0100

    message and conversation domain

---
+++ docroot/WEB-INF/src/com/marcelmika/lims/core/domain/Conversation.java
@@ -0,0 +1,10 @@
+package com.marcelmika.lims.core.domain;
+
+/**
+ * @author Ing. Marcel Mika
+ * @link http://marcelmika.com
+ * Date: 2/7/14
+ * Time: 11:26 PM
+ */
+public class Conversation {
+}
---
+++ docroot/WEB-INF/src/com/marcelmika/lims/events/details/ConversationDetails.java
@@ -0,0 +1,10 @@
+package com.marcelmika.lims.events.details;
+
+/**
+ * @author Ing. Marcel Mika
+ * @link http://marcelmika.com
+ * Date: 2/7/14
+ * Time: 11:34 PM
+ */
+public class ConversationDetails {
+}
