387211a2de74447bd5baaa2a5e4e15b3eb7cff45
Marcel Mika <marcel.mika@rclick.cz>
2014-02-22 16:12:55 +0100

    poller dispatcher refactoring

--- docroot/WEB-INF/src/com/marcelmika/lims/portal/poller/PollerDispatcher.java
+++ docroot/WEB-INF/src/com/marcelmika/lims/portal/poller/PollerDispatcher.java
@@ -10,7 +10,7 @@ import com.marcelmika.lims.events.ResponseEvent;
 /**
  * Poller dispatcher is responsible for calling the appropriate method
  * on poller processor based on the chunkId which is taken from the poller request.
- * ChunkId servers as an identifier of the action which is requested.
+ * ChunkId serves as an identifier of the action which is requested.
  * For example if the user attempts to create a new message the poller request with
  * poller.action.create.message is sent to the poller processor. Based on that the poller
  * dispatcher calls createMessage() method on the poller processor.
@@ -23,26 +23,27 @@ import com.marcelmika.lims.events.ResponseEvent;
 public class PollerDispatcher {
 
     // Actions
-    public static final String POLLER_ACTION_CREATE_MESSAGE = "poller.action.create.message";
-    public static final String POLLER_ACTION_OPEN_CONVERSATION = "poller.action.open.conversation";
-    public static final String POLLER_ACTION_CLOSE_CONVERSATION = "poller.action.close.conversation";
-    public static final String POLLER_ACTION_LEAVE_CONVERSATION = "poller.action.leave.conversation";
-    public static final String POLLER_ACTION_ADD_TO_CONVERSATION = "poller.action.add.to.conversation";
-    public static final String POLLER_ACTION_SEND_MESSAGE = "poller.action.send.message";
-    public static final String POLLER_ACTION_SAVE_SETTINGS = "poller.action.save.settings";
-    public static final String POLLER_ACTION_CHANGE_STATUS = "poller.action.change.status";
-    public static final String POLLER_ACTION_CHANGE_ACTIVE_PANEL = "poller.action.change.active.panel";
-    public static final String POLLER_ACTION_CHANGE_ACTIVE_ROOM_TYPE = "poller.action.change.active.room.type";
-    public static final String POLLER_ACTION_CHAT_ENABLED = "poller.action.chat.enabled";
+    // TODO: refactor, also in js
+    private static final String POLLER_ACTION_CREATE_MESSAGE = "poller.action.create.message";
+    private static final String POLLER_ACTION_OPEN_CONVERSATION = "poller.action.open.conversation";
+    private static final String POLLER_ACTION_CLOSE_CONVERSATION = "poller.action.close.conversation";
+    private static final String POLLER_ACTION_LEAVE_CONVERSATION = "poller.action.leave.conversation";
+    private static final String POLLER_ACTION_ADD_TO_CONVERSATION = "poller.action.add.to.conversation";
+    private static final String POLLER_ACTION_SEND_MESSAGE = "poller.action.send.message";
+    private static final String POLLER_ACTION_SAVE_SETTINGS = "poller.action.save.settings";
+    private static final String POLLER_ACTION_CHANGE_STATUS = "poller.action.change.status";
+    private static final String POLLER_ACTION_CHANGE_ACTIVE_PANEL = "poller.action.change.active.panel";
+    private static final String POLLER_ACTION_CHANGE_ACTIVE_ROOM_TYPE = "poller.action.change.active.room.type";
+    private static final String POLLER_ACTION_CHAT_ENABLED = "poller.action.chat.enabled";
     // Log
     private static Log log = LogFactoryUtil.getLog(PollerProcessor.class);
 
     /**
-     * Calls appropriate method on ChatPollerProcessor based on the chunkId taken from the poller request.
+     * Calls appropriate method on PollerProcessor based on the chunkId taken from the poller request.
      *
      * @param pollerRequest   request from browser
      * @param pollerProcessor poller processor
-     * @return ResponseEvent return by the method
+     * @return ResponseEvent
      * @throws IllegalArgumentException if request does not contain chunkId
      */
     public static ResponseEvent dispatchSendRequest(PollerRequest pollerRequest,
@@ -84,7 +85,7 @@ public class PollerDispatcher {
             responseEvent = pollerProcessor.updateStatus(pollerRequest);
         } // Change active panel
         else if (chunkId.equals(PollerDispatcher.POLLER_ACTION_CHANGE_ACTIVE_PANEL)) {
-            responseEvent = pollerProcessor.changeActivePanel(pollerRequest);
+            responseEvent = pollerProcessor.updateActivePanel(pollerRequest);
         } // Enable/Disable chat
         else if (chunkId.equals(PollerDispatcher.POLLER_ACTION_CHAT_ENABLED)) {
             responseEvent = pollerProcessor.setChatEnabled(pollerRequest);
@@ -101,7 +102,7 @@ public class PollerDispatcher {
 
 
     /**
-     * Calls all appropriate method on ChatPollerProcessor that are scheduled for the receive request event.
+     * Calls all appropriate methods on PollerProcessor that are scheduled for the receive request event.
      *
      * @param pollerRequest   request from browser
      * @param pollerResponse  response sent to browser
