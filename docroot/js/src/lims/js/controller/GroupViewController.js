bb019080bb66391086af033c5a066fee9500683e
Marcel Mika <marcel.mika@rclick.cz>
2014-04-13 21:04:03 +0200

    group controller updated

--- docroot/js/src/lims/js/controller/GroupViewController.js
+++ docroot/js/src/lims/js/controller/GroupViewController.js
@@ -8,24 +8,44 @@ Y.LIMS.Controller.GroupViewController = Y.Base.create('groupViewController', Y.V
     // The initializer runs when a MainController instance is created, and gives
     // us an opportunity to set up the view.
     initializer: function () {
-//        var groupModelList;
+        // Initializations
+        this._initGroups();
+    },
+
+    // Initializes group view list
+    _initGroups: function() {
+        var model;
+
+        model = new Y.LIMS.Model.GroupModelList();
+        model.after('add', this._groupsUpdated, this);
 
         // Init group list
-        this.groupViewList = new Y.LIMS.View.GroupViewList();
-        // Update the display when a new item is added to the list, or when the
-        // entire list is reset.
+        this.groupViewList = new Y.LIMS.View.GroupViewList({
+            container: this.get('groupListContainer'),
+            model: model
+        });
+    },
 
+    // Called whenever the groups model is updated
+    _groupsUpdated: function() {
+        console.log("Groups Updated");
+        this.get('activityIndicator').hide();
     }
-
 
 }, {
 
     // Specify attributes and static properties for your View here.
     ATTRS: {
         // Override the default container attribute.
-        container: {
+        groupListContainer: {
             valueFn: function () {
-                return Y.one("#chat-bar .chat-tabs .buddy-list");
+                return Y.one("#chatBar .buddy-list .panel-content .online-users");
+            }
+        },
+
+        activityIndicator: {
+            valueFn: function () {
+                 return Y.one("#chatBar .buddy-list .panel-content .preloader");
             }
         },
 
--- docroot/js/src/lims/js/view/GroupViewList.js
+++ docroot/js/src/lims/js/view/GroupViewList.js
@@ -7,31 +7,34 @@ Y.namespace('LIMS.View');
 
 Y.LIMS.View.GroupViewList = Y.Base.create('groupViewList', Y.View, [], {
 
-    // This customizes the HTML used for this view's container node.
-    containerTemplate: '<ul class="group-list"/>',
-
     // Specify a model to associate with the view.
     model: Y.LIMS.Model.GroupModelList,
 
     // The initializer runs when the instance is created, and gives
     // us an opportunity to set up the view.
     initializer: function () {
-        var groupModelList;
+        // Init model
+        this._initModel();
+    },
 
+    // Init model attached to the view
+    _initModel: function () {
+        var groupModelList;
         // Init group list
-        groupModelList = this.groupModelList = new Y.LIMS.Model.GroupModelList(); // todo: new this.model()
+        groupModelList = this.get('model');
         // Update the display when a new item is added to the list, or when the
         // entire list is reset.
-        groupModelList.after('add', this.updateGroupViewList, this);
-        groupModelList.after('reset', this.updateGroupViewList, this);
+        groupModelList.after('add', this._updateGroupViewList, this);
+        groupModelList.after('reset', this._updateGroupViewList, this);
 
         // Load saved items from server or local storage
         groupModelList.load();
     },
 
+
     // Creates a new GroupView instance and renders it into the list whenever a
     // Group item is added to the list.
-    updateGroupViewList: function (e) {
+    _updateGroupViewList: function (e) {
         var groupView;
         // Create new Group View Item
         groupView = new Y.LIMS.View.GroupViewItem({model: e.model});
@@ -44,12 +47,13 @@ Y.LIMS.View.GroupViewList = Y.Base.create('groupViewList', Y.View, [], {
 }, {
     // Specify attributes and static properties for your View here.
     ATTRS: {
-        // Override the default container attribute.
+
         container: {
-            valueFn: function () {
-                return Y.one("#chatBar .buddy-list .panel-content .online-users");
-//                return Y.Node.create(this.containerTemplate);
-            }
+            value: null // default value
+        },
+
+        model: {
+            value: null // default value
         }
     }
 });
