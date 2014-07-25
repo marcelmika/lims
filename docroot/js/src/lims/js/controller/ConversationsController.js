168fd19b125f02b8d9bc1f27518a668cc257a2a8
Marcel Mika <marcel.mika@rclick.cz>
2014-07-26 00:21:12 +0200

    conversation controller refactoring

--- docroot/js/src/lims/js/controller/ConversationsController.js
+++ docroot/js/src/lims/js/controller/ConversationsController.js
@@ -12,7 +12,6 @@ Y.LIMS.Controller.ConversationsController = Y.Base.create('conversationsControll
     // element, which will be used as the HTML template for each group item.
     template: Y.one('#lims-conversation-template').get('innerHTML'),
 
-
     // The initializer runs when a MainController instance is created, and gives
     // us an opportunity to set up all sub controllers
     initializer: function () {
@@ -35,102 +34,144 @@ Y.LIMS.Controller.ConversationsController = Y.Base.create('conversationsControll
     },
 
     /**
-     * There may exist opened conversations which are rendered. This function will bind to such
-     * conversations and create appropriate javascript objects based on that.
+     * This is quite important method. Each conversation is represented by an unique string ID.
+     * Such ID is composed by participants screenNames separated by "_" sign. First, we need to sort
+     * both participants names alphabetically and then separate them with "_". If we don't do that we
+     * will have two conversations e.g. A_B and B_A, however both conversations are between the same people.
+     * So if we first sort by the screenName we will always have A_B id for conversation even though B created
+     * the conversation.
+     *
+     * @param firstScreenName
+     * @param secondScreenName
+     * @returns {string} Conversation ID
+     * @private
+     */
+    _generateConversationId: function (firstScreenName, secondScreenName) {
+        // Vars
+        var screenNames = [firstScreenName, secondScreenName];
+
+        // Sort screen names alphabetically
+        screenNames.sort();
+
+        // Return new conversation Id
+        return screenNames[0] + "_" + screenNames[1];
+    },
+
+    /**
+     * There may exist opened conversations which are already rendered in jsp and not via javascript.
+     * This function will bind to such conversations and create appropriate javascript objects.
      *
      * @private
-     * // TODO: Refactor
      */
     _bindConversations: function () {
         // Find all already rendered conversations
-        var conversationNodes = this.get('parentContainer').all('.conversation'),
-            list = this.get('conversationMap'),
-            buddy = this.get('buddyDetails'),
-            listID,
-            conversation,
-            controller;
+        var conversationNodes = this.get('conversationNodes'),  // List of already rendered conversation nodes
+            map = this.get('conversationMap'),                  // Map that holds all conversation controllers
+            buddyDetails = this.get('buddyDetails'),            // Currently logged user
+            conversationId,                                     // Id of the conversation
+            conversationModel,                                  // Model which will be attached to controller
+            controller;                                         // Bind controller
+
         // Create js objects for each node
         conversationNodes.each(function (conversationNode) {
-            listID = conversationNode.attr('data-conversationId');
-            // Check if the conversation is already there
-            if (!list.hasOwnProperty(listID)) {
-                // Create new conversation
-                conversation = new Y.LIMS.Model.ConversationModel({
-                    conversationId: listID,
-                    creator: buddy
+            // Since the conversation was already rendered it contains a conversation ID which we need get
+            // from the attribute on conversation node
+            conversationId = conversationNode.attr('data-conversationId');
+
+            // Bind controller only if it's not in the map
+            if (!map.hasOwnProperty(conversationId)) {
+                // Create new conversation model
+                conversationModel = new Y.LIMS.Model.ConversationModel({
+                    conversationId: conversationId,
+                    creator: buddyDetails
                 });
                 // Create new single user conversation and add it to the list
                 controller = new Y.LIMS.Controller.SingleUserConversationViewController({
-                    controllerId: listID,
+                    // Id of controller is the id of conversation. Thanks to this we can
+                    // set the currently opened panel.
+                    controllerId: conversationId,
                     container: conversationNode,
-                    model: conversation
+                    model: conversationModel
                 });
-                // Add to list
-                list[listID] = controller;
-            } else {
-                controller = list[listID];
+                // Add to map
+                map[conversationId] = controller;
             }
         });
     },
 
     /**
-     * Called whenever the buddy is selected
+     * Called whenever the buddy is selected from conversations.
+     * If a conversation with the particular buddy already exists we
+     * just open the conversation for the user. If not, we first create
+     * new conversation and then present it to the user.
      *
      * @param buddy
      * @private
-     * // TODO: Refactor
      */
     _onBuddySelected: function (buddy) {
-        if (buddy !== undefined) {
-            var controller,
-                conversation,
-                container,
-                list = this.get('conversationMap'),
-            // TODO: Refactor
-                listID = this.get('buddyDetails').get('screenName') + "_" + buddy.get('screenName'); // Id is a screenName of the selected buddy
-            // Check if the conversation is already there
-            if (!list.hasOwnProperty(listID)) {
-                // Create new conversation
-                conversation = new Y.LIMS.Model.ConversationModel({
-                    conversationId: listID,
-                    creator: this.get('buddyDetails'),
-                    participants: [buddy],
-                    title: buddy.get('fullName')
-                });
 
-                container = Y.Node.create(this.containerTemplate);
-                 // TODO: Init container
-                container.set('innerHTML',
-                    Y.Lang.sub(this.template, {
-                        conversationTitle: conversation.get('title'),
-                        triggerTitle: conversation.get('title'),
-                        unreadMessages: conversation.get('unreadMessages')
-                    })
-                );
-                // Add panel container to parent container
-                this.get('parentContainer').append(container);
+        // Vars
+        var map = this.get('conversationMap'),              // Map that holds all conversation controllers
+            buddyDetails = this.get('buddyDetails'),        // Currently logged user
+            conversationId,                                 // Id of the conversation passed to controller
+            conversationModel,                              // Model passed to controller
+            conversationContainer,                          // Container node passed to controller
+            controller;                                     // Controller (selected or newly created)
 
-                conversation.save();
-                // Create new single user conversation and add it to the list
-                controller = new Y.LIMS.Controller.SingleUserConversationViewController({
-                    container: container,
-                    controllerId: listID,
-                    model: conversation
-                });
-                // Add to list
-                list[listID] = controller;
-            } else {
-                controller = list[listID];
-            }
+        // Generate conversation id
+        conversationId = this._generateConversationId(buddyDetails.get('screenName'), buddy.get('screenName'));
 
-            controller.presentViewController();
+        // Such conversation is already in the map
+        if (map.hasOwnProperty(conversationId)) {
+            // Find it, later on we will present it to the user
+            controller = map[conversationId];
         }
+        // No such conversation
+        else {
+            // Create new model
+            conversationModel = new Y.LIMS.Model.ConversationModel({
+                conversationId: conversationId,
+                creator: buddyDetails,
+                participants: [buddy],
+                title: buddy.get('fullName')
+            });
+
+            // Create new container node
+            conversationContainer = Y.Node.create(this.containerTemplate);
+            // Set from template
+            conversationContainer.set('innerHTML',
+                Y.Lang.sub(this.template, {
+                    conversationTitle: conversationModel.get('title'),
+                    triggerTitle: conversationModel.get('title'),
+                    unreadMessages: conversationModel.get('unreadMessages')
+                })
+            );
+            // Add panel container to parent container
+            this.get('container').append(conversationContainer);
+
+            // Create new single user conversation controller
+            controller = new Y.LIMS.Controller.SingleUserConversationViewController({
+                container: conversationContainer,
+                controllerId: conversationId,
+                model: conversationModel
+            });
+
+            // Save the model, thanks to that the conversation will be created on server too.
+            conversationModel.save();
+
+            // Add to list
+            map[conversationId] = controller;
+        }
+
+        // At the end show the controller to the user
+        controller.presentViewController();
     }
 
 }, {
     // Specify attributes and static properties for your View here.
     ATTRS: {
-        // Override the default container attribute.
+
+        // Map holds all currently opened conversation controllers
         conversationMap: {
             value: {} // default value
         },
@@ -141,9 +182,16 @@ Y.LIMS.Controller.ConversationsController = Y.Base.create('conversationsControll
         },
 
         // Main container
-        parentContainer: {
+        container: {
             valueFn: function () {
                 return Y.one('#chatBar .chat-tabs');
+            }
+        },
+
+        // List of already rendered conversation nodes
+        conversationNodes: {
+            valueFn: function () {
+                return this.get('container').all('.conversation');
             }
         }
     }
